96d2c78cf4963d86186a5f6eaf18a7ab666cf21e
Maximilian Aigner <maximilian.aigner@unil.ch>
2022-09-14 10:34:57 +0200

    syntactic changes and line width

--- R/eventlist.R
+++ R/eventlist.R
@@ -1,6 +1,9 @@
 # Constructor
-new_eventlist_from_dt <- function(datetimes, values = NULL, bounds, units = "auto") {
-  if (!is.null(values)) { stopifnot(length(datetimes) == length(values)) }
+new_eventlist_from_dt <- function(datetimes, values = NULL,
+                                  bounds, units = "auto") {
+  if (!is.null(values)) {
+    stopifnot(length(datetimes) == length(values)) 
+  }
   stopifnot(length(bounds) != 2 | is.POSIXt(bounds))
   
   stopifnot(is.POSIXt(datetimes))
@@ -8,45 +11,30 @@ new_eventlist_from_dt <- function(datetimes, values = NULL, bounds, units = "aut
                   time2 = rep(bounds[1], length(datetimes)),
                   units = units)
   
+  units <- attr(dts, "units")
+  
+  num_length <- difftime(bounds[2], bounds[1], units = units)
+  
+  
   structure(list(
     datetimes = datetimes,
     num_times = as.numeric(dts),
     num_unit = attr(dts, "units"),
     bounds = bounds,
+    num_bounds = c(0, as.numeric(num_length)),
     values = values,
     marked = !is.null(values)
   ), class = "eventlist")
 }
 
-# new_eventlist_from_num <- function(nums, num_unit = NULL, values = NULL, bounds) {
-#   stopifnot(length(bounds != 2) | is.numeric(nums))
-#   if (is.null(num_unit)) {
-#     stopifnot(is.numeric(bounds))
-#   }
-#   structure(list(
-#     num_times = nums,
-#     num_unit = num_unit,
-#     bounds = bounds,
-#     values = values
-#   ), class = "eventlist")
-# }
-
-# Validator
-validate_eventlist <- function(el) {
-  if (any(ev$datetimes < el$bounds[1] | ev$datetimes > el$bounds[2])) {
-    stop("Values outside the bounds", call. = FALSE)
-  }
-  
-  return(el)
-}
-
 # Helper constructor
-eventlist <- function(datetimes, values = NULL, bounds, units = "auto") {
+eventlist <- function(datetimes, values = NULL,
+                      bounds, units = "auto") {
   # TODO: Implement creation from other types? File descriptors?
   new_eventlist_from_dt(datetimes, values, bounds, units)
 }
 
-summary.eventlist <- function(el) {
+summary.eventlist <- function(el, ...) {
   cat("Event list spanning ")
   cat(as.character(el$bounds[1]))
   cat(" -- ")
@@ -59,8 +47,8 @@ summary.eventlist <- function(el) {
   }
 }
 
-length.eventlist <- function(el) {
-  return(length(el$datetimes))
+length.eventlist <- function(x) {
+  return(length(x$datetimes))
 }
 
 `[.eventlist` <- function(el, i, j = NULL, type = "dt") {
@@ -69,17 +57,18 @@ length.eventlist <- function(el) {
   } else {
     rval <- NULL
   }
-  #rval = ifelse(el$marked, el$values[i], NULL)
-  if(type == "dt")
+  if (type == "dt")
     return(list(datetimes=el$datetimes[i], values = rval))
   else
     return(list(num_times=el$num_times[i], values = rval))
 }
 
 folds <- function(el, K) {
-  intervals <- seq.POSIXt(el$bounds[1], el$bounds[2], length.out = K + 1)
+  intervals <- seq.POSIXt(el$bounds[1], el$bounds[2],
+                          length.out = K + 1)
   folds <- lapply(1:K, function(j) {
-    indices <- el$datetimes > intervals[j] & el$datetimes < intervals[j+1]
+    indices <- el$datetimes > intervals[j] &
+      el$datetimes < intervals[j+1]
     chunk <- new_eventlist_from_dt(
       datetimes = el$datetimes[indices],
       values = if (el$marked) el$values[indices] else NULL,
