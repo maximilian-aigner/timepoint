85f5c5744429044d4e05c1c6f9d41f0f540b8be3
Maximilian Aigner <maximilian.aigner@unil.ch>
2022-09-23 10:43:54 +0200

    Names fixed

--- R/model.R
+++ R/model.R
@@ -31,7 +31,7 @@ tpmodel.fit.poisson <- function(eventlist, covariates, ilink) {
     lower <- -Inf
     upper <- Inf
   }
-  result <- stats::optim(th0, fn = \(x) -.lhood.unmarked(nt, bds, covariates, x), control = list(trace = 1),
+  result <- stats::optim(th0, fn = \(x) -.lhood.unmarked.poisson(nt, bds, covariates, x), control = list(trace = 1),
                   hessian = TRUE, method = method, lower = lower, upper = upper)
   
   # Result is good but on numeric scale,
@@ -39,7 +39,7 @@ tpmodel.fit.poisson <- function(eventlist, covariates, ilink) {
   return(result)
 }
 
-.lhood.unmarked <- function(ntimes, bds, fs, th) {
+.lhood.unmarked.poisson <- function(ntimes, bds, fs, th) {
   th <- as.matrix(th)
   evs <- sapply(fs, function(f) f(ntimes))
   term1 <- sum(evs %*% th)
@@ -57,7 +57,7 @@ tpmodel.fit.poisson <- function(eventlist, covariates, ilink) {
   return(term1-term2)
 }
 
-.lhood.marked <- function(ntimes, xs, bds, fs, th) {
+.lhood.marked.poisson <- function(ntimes, xs, bds, fs, th) {
   th <- as.matrix(th)
   print(dim(th))
   
@@ -69,7 +69,7 @@ tpmodel.fit.poisson <- function(eventlist, covariates, ilink) {
     evals <- sapply(fs, \(f) f(x[1], x[2:length(x)])) # N times K
     res <- exp(evals%*%th)
     res[is.nan(res) | abs(res) > 1e20] <- 1e20
-    return(res)    
+    return(res)
   }, lowerLimit = bds[c(1,3)], upperLimit = bds[c(2,4)])$integral
   return(term1 - term2)
 }
