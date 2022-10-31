19b8a09d476bb14ebdfeb77fd73aa7ae04503f2f
Maximilian Aigner <maximilian.aigner@unil.ch>
2022-10-31 09:39:30 +0100

    Seasonal plot and fixed axis labelling for bcplot

--- R/plots.R
+++ R/plots.R
@@ -3,6 +3,7 @@
 
 # "Bar code" plot
 bcplot <- function(el, add = FALSE, ...) {
+  stopifnot("not an eventlist!"="eventlist" %in% class(el))
   if (!add) {
     plot.new()
     plot.window(xlim = el$bounds, ylim = c(0, 1))
@@ -11,7 +12,17 @@ bcplot <- function(el, add = FALSE, ...) {
            y1 = rep(1, length(el$datetimes)), ...)
   if (!add) {
     axis.POSIXct(x = el$datetimes, side = 1, cex.axis = 0.8)
-    axis.POSIXct(at = el$bounds, side = 1, line = 2, lwd = 0, format = "%Y-%m-%d %H:%M:%S")
-    title(main = paste("Events from", paste0(el$bounds, collapse = "--"), "\n"), cex.main = 1)
+    axis.POSIXct(at = el$bounds, side = 1, line = 2, lwd = 0,
+                 format = "%Y-%m-%d %H:%M:%S")
+    title(main = paste("Events from", paste0(el$bounds, collapse = "--"), "\n"),
+          cex.main = 1)
   }
+}
+
+seasonplot <- function(el, season_unit, ...) {
+  stopifnot("object not an eventlist" = "eventlist" %in% class(el))
+  
+  rescaled_times <- el$datetimes - floor_date(el$datetimes, season_unit, week_start = 1)
+  plot(rescaled_times, rep(0, length(rescaled_times)), type = 'p', ylim = c(-.5, .5),
+       yaxt = 'n', bty = 'n', ylab = "", xlim = range(rescaled_times))
 }
