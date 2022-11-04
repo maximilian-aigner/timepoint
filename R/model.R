dc369ee500c9673c256ea14d6b2c65eab719e04c
Maximilian Aigner <maximilian.aigner@unil.ch>
2022-11-04 15:10:17 +0100

    Reworking models

--- R/model.R
+++ R/model.R
@@ -1,19 +1,81 @@
-tpmodel <- function(eventlist, covariates, ilink = exp, do.fit = TRUE,
-                    type = "poisson") {
-  stopifnot(type == "poisson")
+tpmodel <- function(cif, mdens, data, ilink = identity,
+                    do.fit = TRUE, num_integral = TRUE,
+                    ...) {
+  stopifnot("eventlist" %in% class(data))
+  if (is.character(cif)) {
+    cif <- cif(cif)
+  }
+  m <- match.call()
+  #mt.t <- terms(formula_times)
+  #mt.m <- terms(formula_marks)
   if (do.fit) {
-    if (type == "poisson")
-      out <- tpmodel.fit.poisson(eventlist, covariates, ilink)
+    pardim <- sum(unlist(cif$pars))
+    
+    nll <- \(par) -.lhood.marked(
+      data$num_times,
+      data$values,
+      data$num_bounds[2],
+      cif
+    )
+    
+    out <- stats::optim(0, fn = nll, ...)
   }
   else
     out <- NULL
   return(structure(list(
-    eventlist = eventlist,
+    data = data,
+    type = type,
+    mdens = mdens,
     covariates = covariates,
-    fitted_model = out,
+    fitted = out,
     ilink = ilink,
-    unit = eventlist$num_unit
   ), class = "tpmodel"))
+}
+
+cif <- function(type, evalfun = NULL, compfun = NULL, dist = NULL) {
+  if (type == "hpp") {
+    pars <- list(lambda = 1)
+    evalfun <- \(t,x,hist,par) par$lambda
+    compfun <- \(t,hist,par) par$lambda*t
+  } else if (type == "renewal") {
+    stopifnot(!is.null(dist))
+    densfun <- get(paste0("d", dist))
+    cumfun <- get(paste0("p", dist))
+    parnames <- setdiff(formalArgs(densfun), c("x", "log"))
+    pars <- as.list(rep(1, length(parnames)))
+    names(pars) <- parnames
+    
+    hfun <- function(x, ...) {
+      densfun(x, ...) / cumfun(x, ..., lower.tail = FALSE)
+    }
+    evalfun <- \(t,x,hist,par) do.call(hfun,args = c(x = t - hist[nrow(hist),1],
+                                              par))
+    intfun <- function(t,hist,par) {
+      diffs <- diff(c(0,hist[,1]))
+      contribs <- sapply(diffs, \(tt) -do.call(cumfun, c(x = tt, par, lower.tail = FALSE, log = TRUE)))
+      last_bit <- -do.call(cumfun, c(x = t - hist[nrow(hist),1], par, lower.tail = FALSE,
+                                     log = TRUE))
+      return(contribs + last_bit)
+    }
+  } else if (type == "scorr") {
+    
+  } else if (type == "hawkes") {
+    
+  } else {
+    # Custom cif
+    stopifnot(!is.null(evalfun) & !is.null(npar))
+    if (is.null(compfun)) {
+      # Use numerical integration:
+      compfun = integrated(evalfun)
+    }
+  }
+  
+  return(structure(list(
+    type = type,
+    evalfun = evalfun,
+    compfun = compfun,
+    pars = pars
+  )))
 }
 
 tpmodel.fit.poisson <- function(eventlist, covariates, ilink) {
@@ -39,9 +101,6 @@ tpmodel.fit.poisson <- function(eventlist, covariates, ilink) {
   }
   result <- stats::optim(th0, fn = ofun, hessian = TRUE, method = method,
                          lower = lower, upper = upper)
-  
-  # Result is good but on numeric scale,
-  # i.e. it matches length(eventlist)/eventlist$num_bounds[2]
   return(result)
 }
 
@@ -80,4 +139,12 @@ tpmodel.fit.poisson <- function(eventlist, covariates, ilink) {
   return(term1 - term2)
 }
 
-
+.lhood.marked <- function(num_times, marks, bT, cif, par) {
+  # hists <- sapply(1:length(num_times))
+  histories <- lapply(1:length(num_times), function(i) cbind(num_times[1:i], marks[1:i]))
+  evals_lg <- mapply(num_times, marks, histories, \(t,x,hist) cif$evalfun(t, x, hist, par))
+  # evals_mdens <- mapply(marks, num_times, mdens)
+  
+  int_lg <- cif$compfun(bT,histories[length(num_times)], par) # - cif$compfun(0)
+  return(sum(log(evals_lg)) - int_lg)
+}
